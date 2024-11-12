edf76f6c7c95d7f2ff1d55f585400f173e5ca985
Christty-constanza <conthibuglioni@gmail.com>
2024-11-12 14:55:53 -0300

    Primer avance: Definimos los elementos de la interfaz.

--- app/src/main/java/com/example/evaluacion/MainActivity.java
+++ app/src/main/java/com/example/evaluacion/MainActivity.java
@@ -2,12 +2,23 @@ package com.example.evaluacion;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+    EditText Peso, Estatura, IMC;
+    Button btnCalcular;
+    Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Peso = findViewById(R.id.txtPeso);
+        Estatura = findViewById(R.id.txtEstatura);
+        IMC = findViewById(R.id.txtImc);
+        btnCalcular = findViewById(R.id.btnCalcular);
+        spinner = findViewById(R.id.spinner);
     }
 }
